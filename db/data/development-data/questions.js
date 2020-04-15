144284c57108063186207993387e2d7a19a284e4
Suneet Misra <emailsuneet@icloud.com>
2020-04-15 22:52:12 +0100

    added data directories and question data for seeding database

---
+++ db/data/development-data/questions.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    category: 'diet',
+    question_text: 'How often do you eat meat and dairy?',
+    option_1: 'Daily',
+    option_2: '1 or 2 times',
+    option_3: '3+ times per week',
+    option_4: 'Not at all',
+  },
+  {
+    category: 'diet',
+    question_text: 'How big are your portion sizes?',
+    option_1: 'Smaller than average',
+    option_2: 'Average',
+    option_3: 'Larger than average',
+    option_4: "I'm not sure",
+  },
+  {
+    category: 'diet',
+    question_text: 'How much food ends up wasted in your household?',
+    option_1: 'None',
+    option_2: '1-5 plates per week',
+    option_3: '6-10 plates per week',
+    option_4: 'More than 10 plates per week',
+  },
+
+  {
+    category: 'diet',
+    question_text:
+      'How often do you eat avocados, asparagus, kiwi fruit or pineapples?',
+    option_1: 'Daily',
+    option_2: '1 or 2 times',
+    option_3: '3+ times per week',
+    option_4: 'Not at all',
+  },
+  {
+    category: 'diet',
+    question_text: 'How often do you eat seasonal veg from Europe?',
+    option_1: 'Daily',
+    option_2: '1 or 2 times',
+    option_3: '3+ times per week',
+    option_4: 'Not at all',
+  },
+]
