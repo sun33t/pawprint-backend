335c79c97bff6657a9c595edaef270cf7f3f08c6
Suneet Misra <emailsuneet@icloud.com>
2020-04-15 23:11:12 +0100

    Created knexfile (gitignored) and migrations schema for creating questions table

---
+++ db/migrations/20200415230645_create_questions_table.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  console.log('creating questions table')
+  return knex.schema.createTable('questions', (questionsTable) => {
+    questionsTable.increments('question_id').primary().notNullable()
+    questionsTable.string('category').notNullable()
+    questionsTable.text('question_text').notNullable()
+    questionsTable.string('option_1').notNullable()
+    questionsTable.string('option_2').notNullable()
+    questionsTable.string('option_3').notNullable()
+    questionsTable.string('option_4').notNullable()
+  })
+}
+
+exports.down = function (knex) {
+  console.log('Removing questions table')
+  return knex.schema.dropTable('questions')
+}
