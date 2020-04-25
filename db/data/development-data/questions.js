fcc88ee2fcba2d5bdaf2d07f7c152d282633c44e
Suneet Misra <emailsuneet@icloud.com>
2020-04-25 15:34:43 +0100

    Reformated questions/js dev Data to reflect new structure for seeding database

--- db/data/development-data/questions.js
+++ db/data/development-data/questions.js
@@ -2,163 +2,342 @@ module.exports = [
   {
     category: 'diet',
     question_text: 'How often do you eat meat and dairy?',
-    option_1: 'Daily',
-    option_2: '1 or 2 times',
-    option_3: '3+ times per week',
-    option_4: 'Not at all',
+    option_1: {
+      text: 'Daily',
+      score: 0.6,
+    },
+    option_2: {
+      text: '1 or 2 times',
+      score: 0.3,
+    },
+    option_3: {
+      text: '3+ times per week',
+      score: 0.1,
+    },
+    option_4: {
+      text: 'Not at all',
+      score: 0,
+    },
   },
   {
     category: 'diet',
     question_text: 'How big are your portion sizes?',
-    option_1: 'Smaller than average',
-    option_2: 'Average',
-    option_3: 'Larger than average',
-    option_4: "I'm not sure",
+    option_1: {
+      text: 'Smaller than average',
+      score: 0.1,
+    },
+    option_2: {
+      text: 'Average',
+      score: 0.3,
+    },
+    option_3: {
+      text: 'Larger than average',
+      score: 0.6,
+    },
+    option_4: {
+      text: "I'm not sure",
+      score: 0,
+    },
   },
   {
     category: 'diet',
     question_text: 'How much food ends up wasted in your household?',
-    option_1: 'None',
-    option_2: '1-5 plates per week',
-    option_3: '6-10 plates per week',
-    option_4: 'More than 10 plates per week',
+    option_1: {
+      text: 'None',
+      score: 0,
+    },
+    option_2: {
+      text: '1-5 plates per week',
+      score: 0.1,
+    },
+    option_3: {
+      text: '6-10 plates per week',
+      score: 0.3,
+    },
+    option_4: {
+      text: 'More than 10 plates per week',
+      score: 0.6,
+    },
   },
-
   {
     category: 'diet',
     question_text:
       'How often do you eat avocados, asparagus, kiwi fruit or pineapples?',
-    option_1: 'Daily',
-    option_2: '1 or 2 times',
-    option_3: '3+ times per week',
-    option_4: 'Not at all',
+    option_1: {
+      text: 'Daily',
+      score: 0.6,
+    },
+    option_2: {
+      text: '1 or 2 times',
+      score: 0.1,
+    },
+    option_3: {
+      text: '3+ times per week',
+      score: 0.3,
+    },
+    option_4: {
+      text: 'Not at all',
+      score: 0,
+    },
   },
   {
     category: 'diet',
     question_text: 'How often do you eat seasonal veg from Europe?',
-    option_1: 'Daily',
-    option_2: '1 or 2 times',
-    option_3: '3+ times per week',
-    option_4: 'Not at all',
+    option_1: {
+      text: 'Daily',
+      score: 0.6,
+    },
+    option_2: {
+      text: '1 or 2 times',
+      score: 0.1,
+    },
+    option_3: {
+      text: '3+ times per week',
+      score: 0.3,
+    },
+    option_4: {
+      text: 'Not at all',
+      score: 0,
+    },
   },
   {
     category: 'travel',
     question_text: 'Travel Question 1?',
-    option_1: 'Option 1',
-    option_2: 'Option 2',
-    option_3: 'Option 3',
-    option_4: 'Option 4',
+    option_1: {
+      text: 'Option 1',
+      score: 0,
+    },
+    option_2: {
+      text: 'Option 2',
+      score: 0.1,
+    },
+    option_3: {
+      text: 'Option 3',
+      score: 0.3,
+    },
+    option_4: {
+      text: 'Option 4',
+      score: 0.6,
+    },
   },
   {
     category: 'travel',
     question_text: 'Travel Question 2?',
-    option_1: 'Option 1',
-    option_2: 'Option 2',
-    option_3: 'Option 3',
-    option_4: 'Option 4',
+    option_1: {
+      text: 'Option 1',
+      score: 0,
+    },
+    option_2: {
+      text: 'Option 2',
+      score: 0.1,
+    },
+    option_3: {
+      text: 'Option 3',
+      score: 0.3,
+    },
+    option_4: {
+      text: 'Option 4',
+      score: 0.6,
+    },
   },
   {
     category: 'travel',
     question_text: 'Travel Question 3?',
-    option_1: 'Option 1',
-    option_2: 'Option 2',
-    option_3: 'Option 3',
-    option_4: 'Option 4',
+    option_1: {
+      text: 'Option 1',
+      score: 0,
+    },
+    option_2: {
+      text: 'Option 2',
+      score: 0.1,
+    },
+    option_3: {
+      text: 'Option 3',
+      score: 0.3,
+    },
+    option_4: {
+      text: 'Option 4',
+      score: 0.6,
+    },
   },
   {
     category: 'travel',
     question_text: 'Travel Question 4?',
-    option_1: 'Option 1',
-    option_2: 'Option 2',
-    option_3: 'Option 3',
-    option_4: 'Option 4',
-  },
-  {
-    category: 'travel',
-    question_text: 'Travel Question 5?',
-    option_1: 'Option 1',
-    option_2: 'Option 2',
-    option_3: 'Option 3',
-    option_4: 'Option 4',
+    option_1: {
+      text: 'Option 1',
+      score: 0,
+    },
+    option_2: {
+      text: 'Option 2',
+      score: 0.1,
+    },
+    option_3: {
+      text: 'Option 3',
+      score: 0.3,
+    },
+    option_4: {
+      text: 'Option 4',
+      score: 0.6,
+    },
   },
   {
     category: 'home',
     question_text: 'Home Question 1?',
-    option_1: 'Option 1',
-    option_2: 'Option 2',
-    option_3: 'Option 3',
-    option_4: 'Option 4',
+    option_1: {
+      text: 'Option 1',
+      score: 0,
+    },
+    option_2: {
+      text: 'Option 2',
+      score: 0.1,
+    },
+    option_3: {
+      text: 'Option 3',
+      score: 0.3,
+    },
+    option_4: {
+      text: 'Option 4',
+      score: 0.6,
+    },
   },
   {
     category: 'home',
     question_text: 'Home Question 2?',
-    option_1: 'Option 1',
-    option_2: 'Option 2',
-    option_3: 'Option 3',
-    option_4: 'Option 4',
+    option_1: {
+      text: 'Option 1',
+      score: 0,
+    },
+    option_2: {
+      text: 'Option 2',
+      score: 0.1,
+    },
+    option_3: {
+      text: 'Option 3',
+      score: 0.3,
+    },
+    option_4: {
+      text: 'Option 4',
+      score: 0.6,
+    },
   },
   {
     category: 'home',
     question_text: 'Home Question 3?',
-    option_1: 'Option 1',
-    option_2: 'Option 2',
-    option_3: 'Option 3',
-    option_4: 'Option 4',
+    option_1: {
+      text: 'Option 1',
+      score: 0,
+    },
+    option_2: {
+      text: 'Option 2',
+      score: 0.1,
+    },
+    option_3: {
+      text: 'Option 3',
+      score: 0.3,
+    },
+    option_4: {
+      text: 'Option 4',
+      score: 0.6,
+    },
   },
   {
     category: 'home',
     question_text: 'Home Question 4?',
-    option_1: 'Option 1',
-    option_2: 'Option 2',
-    option_3: 'Option 3',
-    option_4: 'Option 4',
-  },
-  {
-    category: 'home',
-    question_text: 'Home Question 5?',
-    option_1: 'Option 1',
-    option_2: 'Option 2',
-    option_3: 'Option 3',
-    option_4: 'Option 4',
+    option_1: {
+      text: 'Option 1',
+      score: 0,
+    },
+    option_2: {
+      text: 'Option 2',
+      score: 0,
+    },
+    option_3: {
+      text: 'Option 3',
+      score: 0,
+    },
+    option_4: {
+      text: 'Option 4',
+      score: 0,
+    },
   },
   {
     category: 'other',
     question_text: 'Other Question 1?',
-    option_1: 'Option 1',
-    option_2: 'Option 2',
-    option_3: 'Option 3',
-    option_4: 'Option 4',
+    option_1: {
+      text: 'Option 1',
+      score: 0,
+    },
+    option_2: {
+      text: 'Option 2',
+      score: 0.1,
+    },
+    option_3: {
+      text: 'Option 3',
+      score: 0.3,
+    },
+    option_4: {
+      text: 'Option 4',
+      score: 0.6,
+    },
   },
   {
     category: 'other',
     question_text: 'Other Question 2?',
-    option_1: 'Option 1',
-    option_2: 'Option 2',
-    option_3: 'Option 3',
-    option_4: 'Option 4',
+    option_1: {
+      text: 'Option 1',
+      score: 0,
+    },
+    option_2: {
+      text: 'Option 2',
+      score: 0.1,
+    },
+    option_3: {
+      text: 'Option 3',
+      score: 0.3,
+    },
+    option_4: {
+      text: 'Option 4',
+      score: 0.6,
+    },
   },
   {
     category: 'other',
     question_text: 'Other Question 3?',
-    option_1: 'Option 1',
-    option_2: 'Option 2',
-    option_3: 'Option 3',
-    option_4: 'Option 4',
+    option_1: {
+      text: 'Option 1',
+      score: 0,
+    },
+    option_2: {
+      text: 'Option 2',
+      score: 0.1,
+    },
+    option_3: {
+      text: 'Option 3',
+      score: 0.3,
+    },
+    option_4: {
+      text: 'Option 4',
+      score: 0.6,
+    },
   },
   {
     category: 'other',
     question_text: 'Other Question 4?',
-    option_1: 'Option 1',
-    option_2: 'Option 2',
-    option_3: 'Option 3',
-    option_4: 'Option 4',
-  },
-  {
-    category: 'other',
-    question_text: 'Other Question 5?',
-    option_1: 'Option 1',
-    option_2: 'Option 2',
-    option_3: 'Option 3',
-    option_4: 'Option 4',
+    option_1: {
+      text: 'Option 1',
+      score: 0,
+    },
+    option_2: {
+      text: 'Option 2',
+      score: 0.1,
+    },
+    option_3: {
+      text: 'Option 3',
+      score: 0.3,
+    },
+    option_4: {
+      text: 'Option 4',
+      score: 0.6,
+    },
   },
 ]
