7241892e8e45ce90eabe9eeb89ada10a3cee1788
Suneet Misra <emailsuneet@icloud.com>
2020-04-25 16:12:22 +0100

    Added .uique() to position column

--- db/migrations/20200421084126_create_categories_table.js
+++ db/migrations/20200421084126_create_categories_table.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('categories', (categoriesTable) => {
     categoriesTable.string('category_name').primary().unique().notNullable()
     categoriesTable.string('category_title').notNullable().unique()
-    categoriesTable.integer('position').notNullable()
+    categoriesTable.integer('position').notNullable().unique()
   })
 }
 
