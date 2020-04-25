276f38008618954d2569ce236bf9958da8fcdeef
Suneet Misra <emailsuneet@icloud.com>
2020-04-25 14:12:37 +0100

    Added migrations file for creating the new answers table.

---
+++ db/migrations/20200425132740_create_answers_table.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('answers', (answersTable) => {
+    answersTable.increments('answer_id').primary().notNullable()
+    answersTable
+      .string('question_id')
+      .references('questions.question_id')
+      .notNullable()
+      .onDelete('CASCADE')
+    answersTable.integer('position').notNullable()
+    answersTable.string('answer_text').notNullable()
+    answersTable.float('score').notNullable()
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('answers')
+}
